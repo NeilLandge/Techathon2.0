676f291366a5dbbc2dc0f0c9466452c5319100c8
Rohan <rohansalunkhe700@gmail.com>
2025-02-13 22:59:03 +0530

    assets allocated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import './App.css'
+import ButtonGradient from "./assets/svg/ButtonGradient"
 
 function App(){
     return(
         <>
         <h1 className = "text-3xl font-bold underline">Code like its your last commit!</h1>
+        <ButtonGradient />
         </>
     );
 
