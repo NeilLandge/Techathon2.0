7fe479cc32824aad1066463bc742dd4191532f1e
Rohan <rohansalunkhe700@gmail.com>
2025-02-14 21:19:40 +0530

    header added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from "react-router-dom";
 
-import './index.css'
-import App from './App.jsx'
+import './index.css';
+import App from './App.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
